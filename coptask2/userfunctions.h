fdf458243e4ad714a1867555ab8c6d3a6303e05c
Supreeti Kumari <78208708+SupreetiKumari@users.noreply.github.com>
2022-02-20 14:49:59 +0530

    Update userfunctions.h

--- coptask2/userfunctions.h
+++ coptask2/userfunctions.h
@@ -4,32 +4,22 @@
 #include <math.h>
 using namespace std;
 //this function first does the matrix multiplication and then add the bias vector to it
-vector<vector<float> > fullyconnected(vector<vector<float> > a, vector<vector<float> > b, vector<vector<float> > c)
-{
-    int R1 = a.size();
-    int C1 = a[0].size();
-    int R2 = b.size();
-    int C2 = b[0].size();
-    vector<vector<float> > result(R1, vector<float>(C2, 0.0));
-    for (int i = 0; i < R1; i++)
-    {
-        for (int j = 0; j < C2; j++)
-        {
-            result[i][j] = 0.0;
-            for (int k = 0; k < R2; k++)
-            {
-                result[i][j] += a[i][k] * b[k][j];
+
+void sequential(int row1,int co1,int co2,float inputmatrix[row1][co1],float weightmatrix[co1][co2],float biasmatrix[row1][co2],float outputmatrix[row1][co2]){
+int i,j,k;
+	for(i=0;i<row1;i++){
+        for(j=0;j<co2;j++){
+            for(k=0;k<co1;k++){
+                outputmatrix[i][j]+=inputmatrix[i][k]*weightmatrix[k][j];
             }
         }
+
     }
-    for (int i = 0; i < R1; i++)
-    {
-        for (int j = 0; j < C2; j++)
-        {
-            result[i][j] += c[i][j];
-        }
+    //Addition of bias matrix and product of inputmatrix and weightmatrix
+    for(i=0;i<row1;i++){
+        for(j=0;j<co2;j++){
+            outputmatrix[i][j]+=biasmatrix[i][j];}
     }
-    return result;
 }
 
 // this function is to check if a string is a number or not
@@ -38,8 +28,4 @@ bool isanumber(const string& stride)
     string::const_iterator it = stride.begin();
     while (it != stride.end() && std::isdigit(*it)) ++it;
     return !stride.empty() && it == stride.end();
-}
-
-auto time(){
-    return std::chrono::high_resolution_clock::now();
 }
