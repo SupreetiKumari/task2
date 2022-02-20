34ed2e1771f4b58b53a454f7a56edb24ae6a4550
Arushi29goyal <77873184+Arushi29goyal@users.noreply.github.com>
2022-02-20 23:27:21 +0530

    Update userfunctions.h

--- coptask2/userfunctions.h
+++ coptask2/userfunctions.h
@@ -3,8 +3,7 @@
 #include <vector>
 #include <math.h>
 using namespace std;
-//this function first does the matrix multiplication and then add the bias vector to it
-
+//this function first does the matrix multiplication and then adds the bias vector to it
 void sequential(int row1,int co1,int co2,float inputmatrix[1000][1000],float weightmatrix[1000][1000],float biasmatrix[1000][1000],float outputmatrix[1000][1000]){
 int i,j,k;
 	for(i=0;i<row1;i++){
