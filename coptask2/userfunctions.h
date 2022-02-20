4d47900a25cb63cbb9811c76dd9c4256e41a27f5
Supreeti Kumari <78208708+SupreetiKumari@users.noreply.github.com>
2022-02-20 17:50:57 +0530

    Update userfunctions.h

--- coptask2/userfunctions.h
+++ coptask2/userfunctions.h
@@ -5,7 +5,7 @@
 using namespace std;
 //this function first does the matrix multiplication and then add the bias vector to it
 
-void sequential(int row1,int co1,int co2,float inputmatrix[row1][co1],float weightmatrix[co1][co2],float biasmatrix[row1][co2],float outputmatrix[row1][co2]){
+void sequential(int row1,int co1,int co2,float inputmatrix[1000][1000],float weightmatrix[1000][1000],float biasmatrix[1000][1000],float outputmatrix[1000][1000]){
 int i,j,k;
 	for(i=0;i<row1;i++){
         for(j=0;j<co2;j++){
@@ -22,24 +22,7 @@ int i,j,k;
     }
 }
 
-//Multiplication function
-/*void * multi(void *arg){
- long int num=(long int)arg;
- int a,b,c;
-int start=(num*row1)/4;
-int end= ((num+1)*row1)/4;
- for(a=start;a<end;a++){
-        for(b=0;b<co2;b++){
-           outputmatrix[a][b]=0;
-            for(c=0;c<co1;c++){
-                outputmatrix[a][b]+=inputmatrix[a][c]*weightmatrix[c][b];
-            }
-        }
 
-    }
-	return 0;
-
-}/*
 
 // this function is to check if a string is a number or not
 bool isanumber(const string& stride)
