52c4f5b4eba6753e54fe3687de6a3f571c10138d
Arushi29goyal <77873184+Arushi29goyal@users.noreply.github.com>
2022-02-20 09:49:04 +0000

    Update IO.h

--- coptask2/IO.h
+++ coptask2/IO.h
@@ -18,7 +18,7 @@ void matrixtofile(string &filename, float outputmatrix[1000][1000],int r,int c){
     return;
 }
 
-void vectortofile(string &filename, float outputvector,int r,int c){
+void vectortofile(string &filename,float outputvector[1000],int r,int c){
  int i;
  ofstream file(filename);
  file<<c<<"\n";
@@ -31,3 +31,37 @@ void vectortofile(string &filename, float outputvector,int r,int c){
     file.close();
     return;
 }
+
+void filetomatrix(string &filename, float inputmatrix[1000][1000]){
+	ifstream inputfile;
+        inputfile.open(filename);
+        int i,j,row,co;
+        inputfile>>co;
+        inputfile>>row;
+   	float x;
+       for(j=0;j<co;j++){
+        for(i=0;i<row;i++)
+        {
+        inputfile>>x;
+         inputmatrix[i][j]=x;
+        }
+        }
+   	inputfile.close();
+
+}
+
+void filetovector(string &filename, float inputmatrix[1000]){
+	ifstream inputfile;
+                inputfile.open(filename);
+                int co1,row1;
+                inputfile1>>co1;
+                inputfile1>>row1;
+                int i=0;
+                float x;
+                while (inputfile>>x)
+                 {
+                 inputmatrix[i++]=x;
+                 }
+                 inputfile.close();}
+
+
