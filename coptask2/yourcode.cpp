f3f01002dfd70846749eb52565f08a2a42647b3a
Arushi29goyal <77873184+Arushi29goyal@users.noreply.github.com>
2022-02-20 13:29:34 +0530

    Update yourcode.cpp

--- coptask2/yourcode.cpp
+++ coptask2/yourcode.cpp
@@ -17,6 +17,23 @@ using namespace std;
     return std::chrono::high_resolution_clock::now();
 }*/
 
+void sequential(int row1,int co1,int co2,float inputmatrix[row1][co1],float weightmatrix[co1][co2],float biasmatrix[row1][co2],float outputmatrix[row1][co2]){
+int i,j,k;
+	for(i=0;i<row1;i++){
+        for(j=0;j<co2;j++){
+            for(k=0;k<co1;k++){
+                outputmatrix[i][j]+=inputmatrix[i][k]*weightmatrix[k][j];
+            }
+        }
+
+    }
+    //Addition of bias matrix and product of inputmatrix and weightmatrix
+    for(i=0;i<row1;i++){
+        for(j=0;j<co2;j++){
+            outputmatrix[i][j]+=biasmatrix[i][j];}
+    }
+}
+
 bool checkinputfile(string &filename)
 {
     ifstream inputfile;
