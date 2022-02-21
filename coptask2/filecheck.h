7029787987cc778f94330236bccef35665d7edb9
Supreeti Kumari <78208708+SupreetiKumari@users.noreply.github.com>
2022-02-21 11:20:40 +0530

    Update filecheck.h

--- coptask2/filecheck.h
+++ coptask2/filecheck.h
@@ -1,5 +1,7 @@
 #include <vector>
 #include <math.h>
+#include <fstream>
+#include<string>
 using namespace std;
 // this function checks if the input file is a valid one or not
 bool checkinputfile(string &filename)
